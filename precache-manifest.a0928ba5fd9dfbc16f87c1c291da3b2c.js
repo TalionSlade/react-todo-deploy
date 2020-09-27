d69002a901ea7c5f6f3c19bcbe1e156c45b136f2
TalionSlade <arpan01ghosh1307@gmail.com>
2020-09-27 21:32:56 +0530

    Updates

---
+++ precache-manifest.a0928ba5fd9dfbc16f87c1c291da3b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c78489c516ed777a7b6f1f3bf555c0",
+    "url": "/react-todo-deploy/index.html"
+  },
+  {
+    "revision": "f35be2183c692cb4a1ed",
+    "url": "/react-todo-deploy/static/js/2.e8f49189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-deploy/static/js/2.e8f49189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a5fb9c93442ac6d16f",
+    "url": "/react-todo-deploy/static/js/main.2b9f0fb7.chunk.js"
+  },
+  {
+    "revision": "46d10a4621c8b0ed1f68",
+    "url": "/react-todo-deploy/static/js/runtime-main.6710ecfe.js"
+  }
+]);
